d5bce12eebbe5e5b5128f8a7c15bbba420295dee
PhucDinh2 <120449221+PhucDinh2@users.noreply.github.com>
2023-03-15 16:40:13 +0700

    first commit

---
+++ src/main/java/com/example/Mini/Repository/EmpRepo.java
@@ -0,0 +1,11 @@
+package com.example.Mini.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.Mini.Entity.Employee;
+
+@Repository
+public interface EmpRepo  extends JpaRepository<Employee, Integer>{
+
+}
